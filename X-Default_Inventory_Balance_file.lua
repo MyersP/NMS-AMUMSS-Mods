d1c9901978d4c8e1166e0740978c4654fe27677b
MyersP <Myers.Paul@Gmail.com>
2022-11-06 15:48:14 -0800

    updates to sound files adding new file
    Adding X-Default_Inventory_Balance_file.lua update to sounds All-in-one to 1.08

---
+++ X-Default_Inventory_Balance_file.lua
@@ -0,0 +1,40 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "X-InventoryStoreBalance.pak", 
+["MOD_AUTHOR"]				= "Lowkie",
+["NMS_VERSION"]				= "4.05",
+["MOD_DESCRIPTION"]			= "Inventory updates (GDEFAULTINVENTORYBALANCE.MBIN)",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "DEFAULTINVENTORYBALANCE.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{ "DeconstructRefundPercentage",        "1.0" }, --Default 0.5  
+                                {"PlayerPersonalInventoryTechWidth",     "10" }, --Default "10" 
+                                {"PlayerPersonalInventoryTechHeight",    "6" }, --Default "5" 
+                                {"PlayerPersonalInventoryCargoWidth",    "10" }, --Default "8" 
+                                {"PlayerPersonalInventoryCargoHeight",   "8" }, --Default "6"
+                      
+							}
+						},                        
+					}	
+				},
+			},
+		}
+	}
+}
+
+
+
+
+
+
+
+
